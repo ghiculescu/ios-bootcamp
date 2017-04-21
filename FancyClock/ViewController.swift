776038468c36c392560372fbda18f288cabddf4b
Javan Wood <javanwood@me.com>
2017-04-21 17:39:18 +1000

    Initial Commit

---
+++ FancyClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FancyClock
+//
+//  Created by Javan Wood on 21/4/17.
+//  Copyright © 2017 iOS Bootcamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
