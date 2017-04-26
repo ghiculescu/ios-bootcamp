e1a84a878266f923bbccc5cf01e5d6b111df03a0
Javan Wood <javanwood@me.com>
2017-04-27 09:41:22 +1000

    2.1. Hello world (on console) and timer (without storyboard)

--- FancyClock/ViewController.swift
+++ FancyClock/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("World")
+        
+        dateFormatter.timeStyle = .medium
+        
+        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: view.bounds.width, height: view.bounds.height))
+        label.text = dateFormatter.string(from: Date())
+        label.font = UIFont.systemFont(ofSize: 50.0)
+        label.textAlignment = .center
+        view.addSubview(label)
+        
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            label.text = self?.dateFormatter.string(from: Date())
+        }
     }
 
     override func didReceiveMemoryWarning() {
